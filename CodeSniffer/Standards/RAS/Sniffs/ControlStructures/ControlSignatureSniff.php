afefa8ba9474f650215bb2f090c07fa8fe211785
Nemanja Ognjanovic <ognjanovic@gmail.com>
2014-01-09 13:39:31 +0100

    added RAS standard

---
+++ CodeSniffer/Standards/RAS/Sniffs/ControlStructures/ControlSignatureSniff.php
@@ -0,0 +1,23 @@
+<?php
+class RAS_Sniffs_ControlStructures_ControlSignatureSniff extends PEAR_Sniffs_ControlStructures_ControlSignatureSniff
+{
+    /**
+     * Returns the patterns that this test wishes to verify.
+     *
+     * @return array(string)
+     */
+    protected function getPatterns()
+    {
+        return array(
+                'do {EOL...} while (...);EOL',
+                'while (...) {EOL',
+                'for (...) {EOL',
+                'if (...)EOL...{',
+                'foreach (...) {EOL',
+                'EOL...} else if (...) {EOL',
+                '...}EOL...elseEOL...{',
+                'do {EOL',
+               );
+
+    }//end getPatterns()
+}
